7d87a82c99ef10ae3ad9262c24dce218e76d573a
rsuntk <rsuntk@yukiprjkt.my.id>
2025-07-08 08:21:44 +0700

    mt6768-common: slight refactor
    Signed-off-by: rsuntk <rsuntk@yukiprjkt.my.id>

--- common.mk
+++ common.mk
@@ -1,14 +1,13 @@
 #
 # Copyright (C) 2025 The rsuntk Project
 #
-COMMON_PATH := device/samsung/jdm-mt6768-cmn
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
-$(call inherit-product, $(COMMON_PATH)/device.mk)
+$(call inherit-product, device/samsung/jdm-mt6768-cmn/device.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(COMMON_PATH)/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/jdm-mt6768-cmn/recovery/root,recovery/root)
 
 # Model/OEM
 PRODUCT_BRAND := samsung
